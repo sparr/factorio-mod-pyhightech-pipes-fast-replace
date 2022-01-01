4e4f0741017a8872876b44b0861dfe56dd044654
Clarence "Sparr" Risher <sparr0@gmail.com>
2021-12-31 23:00:41 -0500

    Support pyindustry, Refactor, Bump version

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # factorio-mod-pyhightech-pipes-fast-replace
+Restore fast replace for pipes and underground pipes in PyMods
--- data.lua
+++ data.lua
@@ -1,6 +1,16 @@
 -- pyhightech/prototypes/pipes/ht-pipes.lua puts both tiers of underground
 -- pipes in a separate fast_replaceable_group, so pipes replace pipes and
 -- undergrounds replace undergrounds.
--- Let's restore the vanilla behavior here:
-data.raw["pipe-to-ground"]["pipe-to-ground"].fast_replaceable_group = "pipe"
-data.raw["pipe-to-ground"]["ht-pipes-to-ground"].fast_replaceable_group = "pipe"
+-- ditto pyindustry/prototypes/pipes/niobium-pipes.lua
+-- Let's restore the vanilla behavior:
+for _,entity in ipairs({
+  -- pyhightech
+  "pipe-to-ground",
+  "ht-pipes-to-ground",
+  -- pyindustry
+  "niobium-pipe-to-ground",
+}) do
+  if data.raw["pipe-to-ground"][entity] then
+    data.raw["pipe-to-ground"][entity].fast_replaceable_group = "pipe"
+  end
+end
